f805f1f2adc6666b1ff8b82c335ddb7c181b4e2c
Kevin Svitač <kevin@svit.ac>
2023-10-06 13:50:42 +0200

    Prettier on absolutely everything

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -3,10 +3,10 @@ import app from 'flarum/forum/app';
 app.initializers.add('freeflarum/flarum-arevo-theme', () => {
   if (window.innerWidth > 768) {
     var newDiscussionElement = document.querySelector('.item-newDiscussion');
-    newDiscussionElement.classList.add("new__item-newDiscussion");
-    
+    newDiscussionElement.classList.add('new__item-newDiscussion');
+
     var searchElement = document.querySelector('#header-secondary .item-search');
-    
+
     searchElement.parentNode.insertBefore(newDiscussionElement, searchElement.nextSibling);
   }
 });
